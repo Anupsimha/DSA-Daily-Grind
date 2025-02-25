4451d6d092e019d90e8d47c91086e230faef5120
Anupsimha <anupsimha2@gmail.com>
2025-02-26 01:34:49 +0530

    Started heap....Actually started yesterday only but forgot to commit it....25/2/25

---
+++ GeeksForGeeks/Stacks/KthSmallestElement.cpp
@@ -0,0 +1,19 @@
+// int kthSmallest(vector<int> &arr, int k) {
+//     // code here
+//     int r = arr.size();
+//     priority_queue<int> pq;
+    
+//     for(int i = 0 ; i < k ; i++){
+//         pq.push(arr[i]);
+//     }
+    
+//     for(int i = k ; i < r ; i++){
+//         if(arr[i] < pq.top()){
+//             pq.pop();
+//             pq.push(arr[i]);
+//         }
+//     }
+    
+//     int ans = pq.top();
+//     return ans;
+// }
