48529dd3d348df55b37d28590636721af9ca8fa6
Anupsimha <anupsimha2@gmail.com>
2025-03-08 23:53:19 +0530

    Started workin on Trie's, i was not in any mood to study because of some family issue's about our property dispute, but still glad that studied something....8/3/25(Saturday)

---
+++ Coding Ninjas/Hashmaps/MaximumFrequencyNumber.cpp
@@ -0,0 +1,20 @@
+// int maximumFrequency(vector<int> &arr, int n)
+// {
+//     unordered_map<int , int> m;
+//     int maxFreq = 0;
+//
+//     for(int i = 0 ; i < n ; i++){
+//         m[arr[i]]++;
+//         maxFreq = max(maxFreq , m[arr[i]]);
+//     }
+//
+//     int ans = 0;
+//     for(int i = 0 ; i < n ; i++){
+//         if(maxFreq == m[arr[i]]){
+//             ans = arr[i];
+//             break;
+//         }
+//     }
+//    
+//     return ans;
+// }
