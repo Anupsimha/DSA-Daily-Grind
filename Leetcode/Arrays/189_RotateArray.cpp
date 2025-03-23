cbbd0b00344c72dad799e137d56654f95326d993
Anupsimha <anupsimha2@gmail.com>
2025-03-23 20:01:12 +0530

    solved 4 questions today...Feeling good that started adding more pace on leetcode...23/3/25(Sunday)

---
+++ Leetcode/Arrays/189_RotateArray.cpp
@@ -0,0 +1,10 @@
+// void rotate(vector<int>& nums, int k) {
+//     int n = nums.size();
+//     vector<int> new_arr(n);
+//
+//     for(int i = 0 ; i < n ; i++){
+//         new_arr[(i + k) % n] = nums[i];
+//     }
+//
+//     nums = new_arr;
+// }
---
+++ Leetcode/Arrays/26_RemoveDuplicatesFromSortedArray.cpp
@@ -0,0 +1,13 @@
+// int removeDuplicates(vector<int>& nums) {
+//     int n = nums.size();
+
+//     int unique = 0;
+//     for(int i = 1 ; i < n ; i++){
+//         if(nums[i] != nums[i - 1]){
+//             unique++;
+//             nums[unique] = nums[i];
+//         }
+//     }
+
+//     return unique + 1;
+// }
