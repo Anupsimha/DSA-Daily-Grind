65fcf2571dbe628d63bb4b8f4ad0ccd80c1108b5
Anupsimha <anupsimha2@gmail.com>
2025-03-27 22:44:20 +0530

    Solved 3 questions on leetcode of arrays...27/3/25(Thursday)

---
+++ Leetcode/Arrays/169_MajorityElement.cpp
@@ -0,0 +1,7 @@
+// int majorityElement(vector<int>& nums) {
+//     int n = nums.size();
+//
+//     sort(nums.begin() , nums.end());
+//                                          // The majority element is the middle element in the sorted array
+//     return nums[n / 2];
+// }
