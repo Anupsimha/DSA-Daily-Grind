2dd3048e883f875fc1cb09e85d447d6f691ded4d
Anupsimha <anupsimha2@gmail.com>
2025-04-07 01:55:02 +0530

    Solved only 2 questions....6/4/2025(Sunday)

--- Coding Ninjas/Binary Search/CeilTheFloor.cpp
+++ Coding Ninjas/Binary Search/CeilTheFloor.cpp
@@ -1,14 +1,16 @@
+// Floor : Greatest element smaller than or equal to x
+
 // int floorNum(vector<int> &arr, int n, int x){
 // 	int s = 0 , e = n - 1;
 // 	int ans = -1;
-
+//
 // 	while(s <= e){
 // 		int mid = s + (e - s)/2;
-
+//
 // 		if(arr[mid] == x){
 // 			s = mid + 1;
-// 			ans = arr[mid];
-// 		}
+// 			ans = arr[mid];                  
+// 		}                                               
 // 		else if(arr[mid] > x){
 // 			e = mid - 1;
 // 		}
@@ -17,19 +19,21 @@
 // 			s = mid + 1;
 // 		}
 // 	}
-
+//
 // 	return ans;
 // }
+
+// Ceil : Smallest element greater than or equal to x
 
 // int ceilNum(vector<int> &arr, int n, int x){
 // 	int s = 0 , e = n - 1;
 // 	int ans = -1;
-
+//
 // 	while(s <= e){
 // 		int mid = s + (e - s)/2;
-
+//
 // 		if(arr[mid] == x){
-// 			e= mid - 1;
+// 			e = mid - 1;
 // 			ans = arr[mid];
 // 		}
 // 		else if(arr[mid] < x){
@@ -40,17 +44,17 @@
 // 			e = mid - 1;
 // 		}
 // 	}
-
+//
 // 	return ans;
 // }
 
 
 // pair<int, int> getFloorAndCeil(vector<int> &a, int n, int x) {
-	
+//	
 // 	int lower = floorNum(a , n , x);
 // 	int upper = ceilNum(a , n , x);
-
+//
 // 	pair<int , int> p(lower , upper);
-
+//
 // 	return p;
 // }
---
+++ Leetcode/Binary Search/33_SearchInRotatedSortedArray_I.cpp
@@ -0,0 +1,38 @@
+// int getPivot(vector<int>& nums , int n){
+//     int low = 0, high = n - 1;
+//     while (low < high) {
+//         int mid = low + (high - low) / 2;
+//         if (nums[mid] > nums[high]) {
+//             low = mid + 1;
+//         } else {
+//             high = mid;
+//         }
+//     }
+//     return low;
+// }
+
+// int binarySearch(vector<int>& nums , int st , int e , int target){
+//     while(st <= e){
+//         int mid = st + (e - st)/2;
+
+//         if(nums[mid] == target)  return mid;
+//         else if(nums[mid] > target)  e = mid - 1;
+//         else    st = mid + 1;
+//     }
+//     return -1;
+// }
+
+// int search(vector<int>& nums, int target) {
+//     int n = nums.size();
+
+//     int pivot = getPivot(nums , n);
+//     int ans = -1;
+//     if(target >= nums[pivot] && target <= nums[n - 1]){
+//         ans = binarySearch(nums , pivot , n - 1 ,target);
+//     }
+//     else{
+//         ans = binarySearch(nums , 0 , pivot - 1 ,target);
+//     }
+
+//     return ans;
+// }
