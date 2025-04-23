c77525134a252e0c01daceee3f1b532155d65e73
Anupsimha <anupsimha2@gmail.com>
2025-04-24 00:14:49 +0530

    Solved 1 problem of stacks of date 24/4/2025(Wednesday)

---
+++ Leetcode/Stacks/735_AsteroidCollision.cpp
@@ -0,0 +1,36 @@
+// vector<int> asteroidCollision(vector<int>& asteroids) {
+//     int n = asteroids.size();
+
+//     stack<int> st;
+//     vector<int> ans;
+
+//     for (int asteroid : asteroids) {
+//         bool destroyed = false;
+
+//         while (!st.empty() && st.top() > 0 && asteroid < 0) {
+//             if (abs(asteroid) > abs(st.top())) {
+//                 st.pop();  // current survives, keep checking
+//             } else if (abs(asteroid) == abs(st.top())) {
+//                 st.pop();  // both destroyed
+//                 destroyed = true;
+//                 break;
+//             } else {
+//                 destroyed = true;  // current destroyed
+//                 break;
+//             }
+//         }
+
+//         if (!destroyed) {
+//             st.push(asteroid);
+//         }
+//     }
+
+//     while(st.size()){
+//         ans.push_back(st.top());
+//         st.pop();
+//     }
+
+//     reverse(ans.begin() , ans.end());
+
+//     return ans;
+// }
