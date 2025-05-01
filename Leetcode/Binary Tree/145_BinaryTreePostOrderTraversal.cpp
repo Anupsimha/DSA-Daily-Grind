4fbe980a6aaeb93c6ec7505e1a4aea403f98e7d3
Anupsimha <anupsimha2@gmail.com>
2025-05-01 19:04:20 +0530

    Solved 3 problems of Binary Tree's in leetcode today....1/5/2025(Thursday)

---
+++ Leetcode/Binary Tree/145_BinaryTreePostOrderTraversal.cpp
@@ -0,0 +1,14 @@
+// void PostOrder(TreeNode* root , vector<int>& ans){
+//     if(root == NULL)
+//         return ;
+
+//     PostOrder(root->left , ans);
+//     PostOrder(root->right , ans);
+//     ans.push_back(root->val);
+// }
+
+// vector<int> postorderTraversal(TreeNode* root) {
+//     vector<int> ans;
+//     PostOrder(root , ans);
+//     return ans;
+// }
