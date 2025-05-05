0770aa7e4ad595861abb8381f6a5844f6cd6c19a
Anupsimha <anupsimha2@gmail.com>
2025-05-05 23:39:04 +0530

    Solved two questions today of binary tree in leetcode...5/5/2025(monday)

---
+++ Leetcode/Binary Tree/102_LevelOrderTraversal.cpp
@@ -0,0 +1,59 @@
+// vector<vector<int>> levelOrder(TreeNode* root) {
+//     vector<vector<int>> ans;
+
+//     if(!root)   return ans;
+
+//     queue<TreeNode*> q;
+//     q.push(root);
+
+//     while(!q.empty()){
+//         int size = q.size();
+//         vector<int> level;
+
+//         for(int i = 0 ; i < size ; i++){
+//             TreeNode* temp = q.front();
+//             q.pop();
+//             level.push_back(temp->val);
+//             if(temp->left)  q.push(temp->left);
+//             if(temp->right)  q.push(temp->right);
+//         }
+
+//         ans.push_back(level);
+//     }
+
+//     return ans;
+// }
+
+
+
+//Approach 2 : More Optimized Approach
+
+// vector<vector<int>> levelOrder(TreeNode* root) {
+//     vector<vector<int>> ans;
+//     vector<int> ins;
+
+//     if(!root) return ans;
+
+//     queue<pair<int,TreeNode*>> qu;
+//     qu.push({1,root});
+//     int cur = 1;
+
+//     while(!qu.empty()){
+//         TreeNode* temp = qu.front().second;
+//         int curtemp = qu.front().first;
+
+//         qu.pop();
+
+//         if(curtemp > cur){
+//             cur = curtemp;
+//             ans.push_back(ins);
+//             ins.clear();
+//         }
+
+//         ins.push_back(temp->val);
+//         if(temp->left) qu.push({curtemp+1,temp->left});
+//         if(temp->right) qu.push({curtemp+1,temp->right});
+//     }
+//     ans.push_back(ins);
+//     return ans;
+// }
---
+++ Leetcode/Binary Tree/104_MaximumDepthOfBinaryTree.cpp
@@ -0,0 +1,40 @@
+//Most optimal approach
+
+// int maxDepth(TreeNode* root) {
+//     if(!root)   return 0;
+
+//     int left_depth = maxDepth(root->left);
+//     int right_depth = maxDepth(root->right);
+
+//     return max(left_depth , right_depth) + 1;
+// }
+
+
+
+//My Approach 
+
+// int maxDepth(TreeNode* root) {
+//     vector<vector<int>> binaryTree;
+
+//     if(!root)   return 0;
+
+//     queue<TreeNode*> q;
+//     q.push(root);
+
+//     while(!q.empty()){
+//         int size = q.size();
+//         vector<int> level;
+
+//         for(int i = 0 ; i < size ; i++){
+//             TreeNode* temp = q.front();
+//             q.pop();
+//             level.push_back(temp->val);
+//             if(temp->left)  q.push(temp->left);
+//             if(temp->right)  q.push(temp->right);
+//         }
+
+//         binaryTree.push_back(level);
+//     }
+
+//     return binaryTree.size();
+// }
