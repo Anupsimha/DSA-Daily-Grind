1c39ec6087e50f3f35362f6f3fb292f77723ec25
Anupsimha <anupsimha2@gmail.com>
2025-05-11 02:46:52 +0530

    Solved 2 questions on leetcode today of Binary Search....10/5/2025(Saturday)

---
+++ Leetcode/Binary Tree/534_DiameterOfBinaryTree.cpp
@@ -0,0 +1,15 @@
+// int maxLen;
+// int diameter(TreeNode* root) {
+//     if(root == nullptr) return 0;
+//     if(root->left == nullptr && root->right == nullptr) return 1;
+//     int l = diameter(root->left);
+//     int r = diameter(root->right);
+//     maxLen = max(maxLen, l + r);
+//     return 1 + max(l, r);
+// }
+
+// int diameterOfBinaryTree(TreeNode* root) {
+//     maxLen = 0;
+//     diameter(root);
+//     return maxLen;
+// }
