d56707da2f9dd57031263086fbf1ea20aec02801
Anupsimha <anupsimha2@gmail.com>
2025-05-26 10:44:01 +0530

    Solved two stack problems on leetcode of easy and medium level...26/5/2025(Monday)

---
+++ Leetcode/Stacks/71_SimplifyPaths.cpp
@@ -0,0 +1,63 @@
+// Approach : Using Stacks
+
+// string simplifyPath(string path) {
+//     stack<string> st;
+//     stringstream ss(path);
+//     string token = "";
+
+//     while(getline(ss , token , '/')){
+//         if(token == "" || token == ".") continue;
+
+//         if(token != ".."){
+//             st.push(token);
+//         }
+//         else if(!st.empty()){
+//             st.pop();
+//         }
+//     }
+
+//     if(st.empty())
+//         return "/";
+
+//     string res = "";
+
+//     while(!st.empty()){
+//         res = "/" + st.top() + res;
+//         st.pop();
+//     }
+
+//     return res;
+// }
+
+
+
+
+// Appraoch : Using extra space
+
+// string simplifyPath(string path) {
+//     vector<string> st;
+//     stringstream ss(path);
+//     string token = "";
+
+//     while(getline(ss , token , '/')){
+//         if(token == "" || token == ".") continue;
+
+//         if(token != ".."){
+//             st.push_back(token);
+//         }
+//         else if(!st.empty()){
+//             st.pop_back();
+//         }
+//     }
+
+//     if(st.empty())
+//         return "/";
+
+//     string res = "";
+
+//     for(auto& token: st){
+//         res += "/" + token;
+//     }
+
+//     return res;
+// }
