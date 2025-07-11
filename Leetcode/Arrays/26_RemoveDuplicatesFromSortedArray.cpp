61d9c60554e7fadb67d74f03b269065ff0f6949f
Anupsimha <anupsimha2@gmail.com>
2025-07-11 07:39:42 +0530

    Revised 3 Problems and Implemented it in Java also

--- Leetcode/Arrays/26_RemoveDuplicatesFromSortedArray.cpp
+++ Leetcode/Arrays/26_RemoveDuplicatesFromSortedArray.cpp
@@ -11,3 +11,22 @@
 
 //     return unique + 1;
 // }
+
+
+
+
+// Java Implementation
+
+// public int removeDuplicates(int[] nums) {
+//     int n = nums.length;
+
+//     int unique = 0;
+//     for(int i = 1 ; i < n ; i++){
+//         if(nums[i] != nums[i - 1]){
+//             unique++;
+//             nums[unique] = nums[i];
+//         }
+//     }
+
+//     return unique + 1;
+// }
--- Leetcode/Arrays/31_NextPermutation.cpp
+++ Leetcode/Arrays/31_NextPermutation.cpp
@@ -23,3 +23,48 @@
 
 //     reverse(nums.begin() + pointingIndex + 1 , nums.end());
 // }
+
+
+
+
+
+// Java Implementation
+
+// public void nextPermutation(int[] nums) {
+//     int n = nums.length;
+
+//     int pointingIndex = -1;
+//     for(int i = n - 1 ; i > 0 ; i--){
+//         if(nums[i] > nums[i - 1]){
+//             pointingIndex = i - 1;
+//             break;
+//         }
+//     }   
+
+//     if(pointingIndex != -1){
+//         int swapIndex = pointingIndex;
+//         for(int j = n - 1; j >= pointingIndex ; j--){
+//             if(nums[j] > nums[pointingIndex]){
+//                 swapIndex = j;
+//                 break;
+//             }
+//         }
+//         swap(nums , swapIndex , pointingIndex);
+//     }
+
+//     reverse(nums , pointingIndex + 1 , n - 1);
+// }
+
+// private void swap(int[] nums, int i, int j) {
+//     int temp = nums[i];
+//     nums[i] = nums[j];
+//     nums[j] = temp;
+// }
+
+// private void reverse(int[] nums, int start, int end) {
+//     while (start < end) {
+//         swap(nums, start, end);
+//         start++;
+//         end--;
+//     }
+// }
