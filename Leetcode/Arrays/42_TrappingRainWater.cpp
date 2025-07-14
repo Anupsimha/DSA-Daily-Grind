b17d807a069677804028f4c3a4f2a8ffd530f278
Anupsimha <anupsimha2@gmail.com>
2025-07-14 08:20:52 +0530

    Revised some DSA questions

--- Leetcode/Arrays/42_TrappingRainWater.cpp
+++ Leetcode/Arrays/42_TrappingRainWater.cpp
@@ -2,22 +2,22 @@
 
 // int trap(vector<int>& height) {
 //     vector<int> left_max(height.size()) , right_max(height.size());
-
+//
 //     left_max[0] = height[0];
 //     for(int i = 1 ; i < height.size() ; i++){
 //         left_max[i] = max(left_max[i-1] , height[i]);
 //     }
-
+//
 //     right_max[height.size() - 1] = height[height.size() - 1];
 //     for(int i = height.size() - 2 ; i >= 0 ; i--){
 //         right_max[i] = max(right_max[i+1] , height[i]);
 //     }
-
+//
 //     int trapped_water = 0;
 //     for(int i = 0 ; i < height.size() ; i++){
 //         trapped_water += min(left_max[i] , right_max[i]) - height[i]; 
 //     }
-
+//
 //     return trapped_water;
 // }
 
@@ -27,7 +27,7 @@
 
 // int trap(vector<int>& height) {
 //     int n = height.size();
-
+//
 //     int leftmax = 0 , rightmax = 0 , total = 0;
 //     int l = 0 , r = n - 1;
 //     while(l <= r){
