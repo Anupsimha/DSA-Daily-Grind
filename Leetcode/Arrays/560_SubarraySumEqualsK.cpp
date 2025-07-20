f4641ccbe21e2f939b8d1ac5554234921992a41c
Anupsimha <anupsimha2@gmail.com>
2025-07-21 02:31:02 +0530

    Solved one leetcode medium level question

---
+++ Leetcode/Arrays/560_SubarraySumEqualsK.cpp
@@ -0,0 +1,19 @@
+// int subarraySum(vector<int>& nums, int k) {
+//     unordered_map<int, int> prefixSumCount;
+//     prefixSumCount[0] = 1; 
+
+//     int count = 0;
+//     int sum = 0;
+
+//     for(int num : nums) {
+//         sum += num;
+
+//         if(prefixSumCount.find(sum - k) != prefixSumCount.end()) {
+//             count += prefixSumCount[sum - k];
+//         }
+
+//         prefixSumCount[sum]++;
+//     }
+
+//     return count;
+// }
