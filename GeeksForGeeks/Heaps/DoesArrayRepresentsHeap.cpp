4661de16f2914a2251d6dbcc2714f31e0b2aea7b
Anupsimha <anupsimha2@gmail.com>
2025-07-25 23:50:24 +0530

    Solved one question of Heaps

---
+++ GeeksForGeeks/Heaps/DoesArrayRepresentsHeap.cpp
@@ -0,0 +1,12 @@
+// bool isMaxHeap(int arr[], int n) {
+//     for(int i = 0 ; i < n ; i++){
+//         int leftChild = 2 * i + 1;
+//         int rightChild = 2 * i + 2;
+        
+//         if((leftChild < n && arr[i] < arr[leftChild]) || (rightChild < n && 
+//         arr[i] < arr[rightChild]))
+//             return false;
+//     }
+    
+//     return true;
+// }
