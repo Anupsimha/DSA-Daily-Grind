58ac06c283851550e3fc57318889ef7ab6b83cc8
Anupsimha <anupsimha2@gmail.com>
2025-08-04 20:08:18 +0530

    Revised graph question solved on codingNinjas

--- Coding Ninjas/Graphs/ImplementingaGraph.cpp
+++ Coding Ninjas/Graphs/ImplementingaGraph.cpp
@@ -1,18 +1,18 @@
 // vector < vector < int >> printAdjacency(int n, int m, vector < vector < int >> & edges) {
-//     vector<int> ans[n];
+//     vector<int> ans[n];                          // behaves like an array of vectors(jagged array)
 //     // ans array will store all adjacent nodes
 //     for(int i = 0 ; i < m ; i++){
 //         int u = edges[i][0];
 //         int v = edges[i][1];
-
+//
 //         ans[u].push_back(v);
 //         ans[v].push_back(u);
 //     }
-
+//
 //     vector<vector<int>> adj(n);
 //     for(int i = 0 ; i < n ; i++){
 //         adj[i].push_back(i);
-
+//
 //         // entering neighbouring nodes
 //         for(int j = 0 ; j < ans[i].size() ; j++){
 //             adj[i].push_back(ans[i][j]);
