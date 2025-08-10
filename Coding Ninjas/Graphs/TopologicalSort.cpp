7b627df94e110d50a1c3cf44e5920b6d9a8a0933
Anupsimha <anupsimha2@gmail.com>
2025-08-10 17:35:04 +0530

    Solved Cycle Detection in Directed Graph and Topological Sort using DFS

---
+++ Coding Ninjas/Graphs/TopologicalSort.cpp
@@ -0,0 +1,44 @@
+// #include <bits/stdc++.h> 
+// #include <unordered_map>
+// #include <stack>
+// #include <list>
+
+// void topSort(int node , vector<bool> &visited , stack<int> &s , 
+//     unordered_map<int , list<int>>& adj){
+//         visited[node] = 1;
+
+//         for(auto neighbour : adj[node]){
+//             if(!visited[neighbour]){
+//                 topSort(neighbour , visited , s , adj);
+//             }
+//         }
+
+//         s.push(node);
+//     }
+
+// vector<int> topologicalSort(vector<vector<int>> &edges, int v, int e)  {
+//     unordered_map<int , list<int>> adj;
+//     for(int i = 0 ; i < edges.size() ; i++){
+//         int u = edges[i][0];
+//         int v = edges[i][1];
+
+//         adj[u].push_back(v);
+//     }
+
+//     // call dfs topological sort util function for all components
+//     vector<bool> visited(v);
+//     stack<int> s;
+//     for(int i = 0 ; i < v ; i++){
+//         if(!visited[i]){
+//             topSort(i , visited , s , adj);
+//         }
+//     }
+
+//     vector<int> ans;
+//     while(!s.empty()){
+//         ans.push_back(s.top());
+//         s.pop();
+//     }
+
+//     return ans;
+// }
