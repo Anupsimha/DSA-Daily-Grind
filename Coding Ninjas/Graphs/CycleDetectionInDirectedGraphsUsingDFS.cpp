3031523a677d5e74067e8c4e84333ad80fa3bfcf
Anupsimha <anupsimha2@gmail.com>
2025-08-11 05:34:22 +0530

    Revised two graph's problem

--- Coding Ninjas/Graphs/CycleDetectionInDirectedGraphsUsingDFS.cpp
+++ Coding Ninjas/Graphs/CycleDetectionInDirectedGraphsUsingDFS.cpp
@@ -5,12 +5,12 @@
 
 // #include<unordered_map>
 // #include<list>
-
+//
 // bool checkCycleDFS(int node , unordered_map<int , bool>& visited , unordered_map<int , bool>& dfsVisited 
 // , unordered_map<int , list<int>>& adj){
 //   visited[node] = true;
 //   dfsVisited[node] = true;
-
+//
 //   for(auto &neighbour : adj[node]){
 //     if(!visited[neighbour]){
 //       bool cycleDetected = checkCycleDFS(neighbour , visited , dfsVisited , adj);
@@ -22,21 +22,21 @@
 //       return true;
 //     }
 //   }
-
+//
 //   dfsVisited[node] = false;
 //   return false;
 // }
-
+//
 // int detectCycleInDirectedGraph(int n, vector < pair < int, int >> & edges) {
 //   unordered_map<int , list<int>> adj;
-
+//
 //   for(int i = 0 ; i < edges.size() ; i++){
 //     int u = edges[i].first;
 //     int v = edges[i].second;
-
+//
 //     adj[u].push_back(v);
 //   }
-
+//
 //   // call dfs for all componenets
 //   unordered_map<int , bool> visited;
 //   unordered_map<int , bool> dfsVisited;
@@ -47,6 +47,6 @@
 //         return 1;
 //     }
 //   }
-
+//
 //   return 0;
 // }
--- Coding Ninjas/Graphs/TopologicalSort.cpp
+++ Coding Ninjas/Graphs/TopologicalSort.cpp
@@ -6,25 +6,25 @@
 // void topSort(int node , vector<bool> &visited , stack<int> &s , 
 //     unordered_map<int , list<int>>& adj){
 //         visited[node] = 1;
-
+//
 //         for(auto neighbour : adj[node]){
 //             if(!visited[neighbour]){
 //                 topSort(neighbour , visited , s , adj);
 //             }
 //         }
-
+//
 //         s.push(node);
 //     }
-
+//
 // vector<int> topologicalSort(vector<vector<int>> &edges, int v, int e)  {
 //     unordered_map<int , list<int>> adj;
 //     for(int i = 0 ; i < edges.size() ; i++){
 //         int u = edges[i][0];
 //         int v = edges[i][1];
-
+//
 //         adj[u].push_back(v);
 //     }
-
+//
 //     // call dfs topological sort util function for all components
 //     vector<bool> visited(v);
 //     stack<int> s;
@@ -33,12 +33,12 @@
 //             topSort(i , visited , s , adj);
 //         }
 //     }
-
+//
 //     vector<int> ans;
 //     while(!s.empty()){
 //         ans.push_back(s.top());
 //         s.pop();
 //     }
-
+//
 //     return ans;
 // }
