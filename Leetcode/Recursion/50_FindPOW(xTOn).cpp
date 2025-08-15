172c2a8a265edfaedaf4efed6f8feea30a670aac
Anupsimha <anupsimha2@gmail.com>
2025-08-15 20:06:55 +0530

    Solved a recusrion question

---
+++ Leetcode/Recursion/50_FindPOW(xTOn).cpp
@@ -0,0 +1,21 @@
+// Approach : Using Divide and Conquer
+// Time Complexity : O(log n)
+
+// double powRecur(double x , long long n){
+//     if (n == 0) return 1.0;
+
+//     double half = powRecur(x, n / 2);
+//     if (n % 2 == 0)
+//         return half * half;
+//     else
+//         return half * half * x;
+// }
+
+// double myPow(double x, int n) {
+//     long long N = n; 
+//     if (N < 0) {
+//         x = 1 / x;
+//         N = -N;
+//     }
+//     return powRecur(x, N);
+// }
