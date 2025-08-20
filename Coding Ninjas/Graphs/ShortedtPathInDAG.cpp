b0a50c1961a5a0fc1fc7e53dd728186a584c3d69
Anupsimha <anupsimha2@gmail.com>
2025-08-20 19:10:38 +0530

    Revised 2 problem's of Graph

--- Coding Ninjas/Graphs/ShortedtPathInDAG.cpp
+++ Coding Ninjas/Graphs/ShortedtPathInDAG.cpp
@@ -1,6 +1,6 @@
-// // Time Complexity : O(n + e)
-
-
+// Time Complexity : O(n + e)
+//
+//
 // #include <iostream>
 // #include <unordered_map>
 // #include <list>
@@ -8,16 +8,16 @@
 // #include <vector>
 // #include <climits>
 // using namespace std;
-
+//
 // class Graph{
 //     public : 
 //         unordered_map<int , list<pair<int , int>>> adj;
-        
+//        
 //         void addEdge(int u , int v , int weight){
 //             pair<int , int> p = make_pair(v , weight);
 //             adj[u].push_back(p);
 //         }
-        
+//        
 //         void printAdj(){
 //             for(auto i : adj){
 //                 cout<<i.first<<" -> ";
@@ -27,10 +27,10 @@
 //                 cout<<endl;
 //             }
 //         }
-        
+//        
 //         void dfs(int node , unordered_map<int , bool> &vis , stack<int> &topo){
 //             vis[node] = true;
-            
+//            
 //             for(auto neighbour : adj[node]){
 //                 if(!vis[neighbour.first]){
 //                     dfs(neighbour.first , vis , topo);
@@ -38,13 +38,13 @@
 //             }
 //             topo.push(node);
 //         }
-        
+//        
 //         void getShortestPath(int src , vector<int> &dist , stack<int> &topo){
 //             dist[src] = 0;
 //             while(!topo.empty()){
 //                 int top = topo.top();
 //                 topo.pop();
-                    
+//
 //                 if(dist[top] != INT_MAX){
 //                     for(auto i : adj[top]){
 //                         if(dist[top] + i.second < dist[i.first]){
@@ -54,11 +54,11 @@
 //                 }
 //             }
 //         }
-    
+//    
 // };
-
+//
 // int main() {
-    
+//    
 //     Graph g;
 //     g.addEdge(0 , 1 , 5);
 //     g.addEdge(0 , 2 , 3);
@@ -69,10 +69,10 @@
 //     g.addEdge(2 , 5 , 2);
 //     g.addEdge(3 , 4 , -1);
 //     g.addEdge(4 , 5 , -2);
-    
+//    
 //     g.printAdj();
 //     int n = 6;
-    
+//    
 //     //topological sort
 //     unordered_map<int , bool> visited;
 //     stack<int> topo;
@@ -81,20 +81,20 @@
 //             g.dfs(i , visited , topo);
 //         }
 //     }
-    
+//    
 //     int src = 1;
 //     vector<int> dist(n);
 //     for(int i = 0 ; i < n ; i++){
 //         dist[i] = INT_MAX;
 //     }
-    
+//    
 //     g.getShortestPath(src , dist , topo);
-    
+//   
 //     cout<<"Anser is : "<<endl;
 //     for(int i = 0 ; i < n ; i++){
 //         cout<<dist[i]<<" ";
 //     }
 //     cout<<endl;
-
+//
 //     return 0;
 // }
