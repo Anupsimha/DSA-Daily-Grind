c41eb1027cbe6de1a1b8af2067bb911078fffe9a
Anupsimha <anupsimha2@gmail.com>
2025-08-21 05:42:26 +0530

    Solved 2 quesitons of Arrays and 1 problem of basic maths question

---
+++ Leetcode/Arrays/118_Pascal'sTriangle.cpp
@@ -0,0 +1,12 @@
+// vector<vector<int>> generate(int numRows) {
+//     vector<vector<int>> ans;
+//     int n = numRows;
+//     for(int i = 0 ; i<n ; i++){
+//         vector<int> sub_ans(i + 1, 1);  
+//         for(int j = 1 ; j < i ; j++){
+//             sub_ans[j] = ans[i-1][j-1] + ans[i-1][j];
+//         }
+//         ans.push_back(sub_ans);
+//     }
+//     return ans;
+// }
---
+++ Leetcode/Binary Search/81_SearchInRotatedSortedArrayII.cpp
@@ -0,0 +1,42 @@
+// int getPivot(vector<int> &nums , int n){
+//     int s = 0 , e = nums.size() - 1;
+//     while(s < e && nums[s] == nums[s + 1])
+//         s++;
+
+//     int mid = s + (e-s)/2;
+//     while(s < e){
+//         if(nums[mid] > nums[e])
+//             s = mid + 1;
+//         else
+//             e = mid;
+        
+//         mid = s + (e-s)/2;
+//     }
+
+//     return s;
+// }
+
+// bool binarySearch(vector<int>& nums , int st , int e , int target){
+//     while(st <= e){
+//         int mid = st + (e - st)/2;
+
+//         if(nums[mid] == target)
+//             return true;
+//         else if(nums[mid] < target)
+//             st = mid + 1;
+//         else
+//             e = mid - 1;
+//     }
+//     return false;
+// }
+
+// bool search(vector<int>& nums, int target) {
+//     int n = nums.size();
+
+//     int pivot = getPivot(nums , n);
+
+//     if(target >= nums[pivot] && target <= nums[nums.size() - 1])
+//         return binarySearch(nums , pivot , nums.size() - 1 , target);
+//     else
+//         return binarySearch(nums , 0 , pivot - 1 , target);
+// }
