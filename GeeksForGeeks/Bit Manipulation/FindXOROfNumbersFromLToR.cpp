8d890eea2d88933abb6835de9879aaf7a621fe3a
Anupsimha <anupsimha2@gmail.com>
2025-09-19 05:01:00 +0530

    Solved 2 questions of Bit Manipulaiton

---
+++ GeeksForGeeks/Bit Manipulation/FindXOROfNumbersFromLToR.cpp
@@ -0,0 +1,10 @@
+// int xor1ToN(int n){
+//     if(n % 4 == 1)  return 1;
+//     else if(n % 4 == 2) return n + 1;
+//     else if(n % 4 == 3) return 0;
+//     else return n;
+// }
+
+// int findXOR(int l, int r) {
+//     return xor1ToN(l - 1) ^ xor1ToN(r);
+// }
