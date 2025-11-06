d36689fe28767aa86bc28e8b9008a935027014fe
Anupsimha <anupsimha2@gmail.com>
2025-11-07 02:30:57 +0530

    Solved 2 more questions

---
+++ Leetcode/Dynamic Programming/1911_2D_MaximumAlternatingSum.cpp
@@ -0,0 +1,66 @@
+// Approach : Recursion
+
+// typedef long long ll;
+// int n;
+
+// ll solve(int idx , vector<int>& nums , bool flag){
+//     if(idx >= n)
+//         return 0;
+
+//     ll skip = solve(idx + 1 , nums , flag);
+
+//     ll val = nums[idx];
+//     if(!flag)
+//         val = -val;
+
+//     ll take = solve(idx + 1 , nums , !flag) + val;
+
+//     return max(take , skip);
+// }
+
+// long long maxAlternatingSum(vector<int>& nums) {
+//     n = nums.size();
+
+//     return solve(0 , nums , true);    
+// }
+
+
+
+
+// Approach : Recursion + Memoization
+
+// typedef long long ll;
+// int n;
+// ll t[1000001][2];
+
+// ll solve(int idx , vector<int>& nums , bool flag){
+//     if(idx >= n)
+//         return 0;
+
+//     if(t[idx][flag] != -1)
+//         return t[idx][flag];
+
+//     ll skip = solve(idx + 1 , nums , flag);
+
+//     ll val = nums[idx];
+//     if(!flag)
+//         val = -val;
+
+//     ll take = solve(idx + 1 , nums , !flag) + val;
+
+//     return t[idx][flag] = max(take , skip);
+// }
+
+// long long maxAlternatingSum(vector<int>& nums) {
+//     n = nums.size();
+
+//     memset(t , -1 , sizeof(t));
+
+//     return solve(0 , nums , true);    
+// }
+
+
+
+
+// Approach : Bottom-Up (Tabulation)
+
