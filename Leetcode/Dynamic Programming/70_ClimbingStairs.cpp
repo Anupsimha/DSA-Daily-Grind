60c4844a6eadaa15224cad01516a27acb6134ef3
Anupsimha <anupsimha2@gmail.com>
2025-11-27 18:29:08 +0530

    Solved 2 DSA questions

--- Leetcode/Dynamic Programming/70_ClimbingStairs.cpp
+++ Leetcode/Dynamic Programming/70_ClimbingStairs.cpp
@@ -1,3 +1,29 @@
+// Approach : Recursion
+
+// typedef long long ll; 
+
+// ll solve(int n){
+//     if(n == 0)
+//         return 1;
+
+//     if(n < 0)
+//         return 0;
+
+//     return solve(n - 1) + solve(n - 2);
+// }
+
+// int climbStairs(int n) {
+//     return (int)solve(n);
+// }
+
+
+
+
+
+// Approach : Recursion + Memoization
+// Time Complexity : O(n)
+// Space Complexity : O(n)
+
 // typedef long long ll; 
 
 // ll solve(int n , int i , vector<ll> &dp){
@@ -22,4 +48,58 @@
 //     ll ans = solve(n , 0 , dp);
 //
 //     return (int)ans;
+// }
+
+
+
+
+
+// Approach : Tabulation
+// Time Complexity : O(n)
+// Space Complexity : O(n)
+
+// typedef long long ll; 
+
+// ll solve(int n){
+//     vector<ll> dp(n + 1 , 0);
+//     dp[0] = 1;
+//     dp[1] = 1;
+
+//     for(int i = 2 ; i <= n ; i++){
+//         dp[i] = dp[i - 1] + dp[i - 2];
+//     }
+
+//     return dp[n];
+// }
+
+// int climbStairs(int n) {
+//     return (int)solve(n);
+// }
+
+
+
+
+
+// Approach : Space Optimization
+// Time Complexity : O(n)
+// Space Complexity : O(1)
+
+// typedef long long ll; 
+
+// ll solve(int n){
+//     int prev1 = 1;
+//     int prev2 = 1;
+
+//     for(int i = 2 ; i <= n ; i++){
+//         int curr = prev1 + prev2;
+
+//         prev2 = prev1;
+//         prev1 = curr;
+//     }
+
+//     return prev1;
+// }
+
+// int climbStairs(int n) {
+//     return (int)solve(n);
 // }
--- Leetcode/Dynamic Programming/91_DecodeWays.cpp
+++ Leetcode/Dynamic Programming/91_DecodeWays.cpp
@@ -1,3 +1,70 @@
+// Approach : Recursion 
+// Time Complexity : Exponential
+// Space Complexity : O(n)
+
+// int solve(string &s , int &n , int i){
+//     if(i == n)
+//         return 1;
+
+//     if(s[i] == '0')
+//         return 0;
+
+//     int curr_char_possibilities = solve(s , n , i + 1);
+
+//     int next_char_possibilities = 0;
+//     if(i + 1 < n && (s[i] == '1' || (s[i] == '2' && s[i + 1] <= '6'))) // check is next number makes less than 26
+//         next_char_possibilities = solve(s , n , i + 2);
+
+//     return curr_char_possibilities + next_char_possibilities;
+// }
+
+// int numDecodings(string s) {
+//     int n = s.length();
+    
+//     return solve(s , n , 0);
+// }
+
+
+
+
+// Approach : Recursion + Memoization
+// Time Complexity : O(n)
+// Space Complexity : O(n)
+
+// int solve(string &s , int &n , int i , vector<int>& dp){
+//     if(i == n)
+//         return 1;
+
+//     if(s[i] == '0')
+//         return 0;
+
+//     if(dp[i] != -1)
+//         return dp[i];
+
+//     int curr_char_possibilities = solve(s , n , i + 1 , dp);
+
+//     int next_char_possibilities = 0;
+//     if(i + 1 < n && (s[i] == '1' || (s[i] == '2' && s[i + 1] <= '6'))) // check is next number makes less than 26
+//         next_char_possibilities = solve(s , n , i + 2 , dp);
+
+//     return dp[i] = curr_char_possibilities + next_char_possibilities;
+// }
+
+// int numDecodings(string s) {
+//     int n = s.length();
+//     vector<int> dp(n + 1 , -1);
+    
+//     return solve(s , n , 0 , dp);
+// }
+
+
+
+
+
+// Approach : Tabulation
+// Time Complexity : O(n)
+// Space Complexity : O(n)
+
 // int numDecodings(string s) {
 //     if(s[0] == '0')
 //         return 0;
